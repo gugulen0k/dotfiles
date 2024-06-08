8577377308a683422bb86959621359625c2b5919
gugulenok <terzi.kirill.ceiti@gmail.com>
2024-06-08 16:47:53 +0300

    Refactored vim-fugitive plugin

--- .config/nvim/lua/gugulenok/core/keymaps.lua
+++ .config/nvim/lua/gugulenok/core/keymaps.lua
@@ -5,21 +5,3 @@ vim.g.mapleader = ' '
 utils.map('', '<leader><leader>', ':e #<CR>') -- Switching between last two files
 utils.map('n', '<C-s>', ':w<CR>')
 utils.map('n', 'Q', '<nop>')
-
--- Function to toggle :G in a vertical split
-local function toggle_git_status()
-  -- Check if any fugitive buffer is open
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    local bufname = vim.api.nvim_buf_get_name(buf)
-    if bufname:match('^fugitive://') then
-      -- Close the buffer if found
-      vim.api.nvim_buf_delete(buf, { force = true })
-      return
-    end
-  end
-  -- If no fugitive buffer is found, open it in a vertical split
-  vim.cmd('vertical G')
-end
-
--- Map <space>G to toggle_git_status
-utils.map('n', '<space>G', toggle_git_status, { noremap = true, silent = true })
--- .config/nvim/lua/gugulenok/plugins/init.lua
+++ .config/nvim/lua/gugulenok/plugins/init.lua
@@ -2,7 +2,6 @@ return {
   "christoomey/vim-tmux-navigator",     -- Navigate inside neovim like in tmux
   "nvim-lua/plenary.nvim",              -- Useful lua functions used by lots of plugins
   "tpope/vim-surround",
-  "tpope/vim-fugitive",
   "tpope/vim-endwise",
   "tpope/vim-repeat",
   "vim-ruby/vim-ruby",
