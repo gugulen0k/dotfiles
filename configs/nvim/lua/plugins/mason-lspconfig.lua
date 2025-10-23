a9242d1d2a83e03c75e61021d73834554bae09ed
gugulenok <terzi.kirill.ceiti@gmail.com>
2025-10-23 20:28:16 +0300

    Added vue lsp & update ghostty config

--- configs/nvim/lua/plugins/mason-lspconfig.lua
+++ configs/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,21 +1,56 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		ensure_installed = {
-			"solargraph",
-			"vue_ls",
-			"ts_ls",
-			"pyright",
-			"zls",
-			"html",
-			"cssls",
-			"tailwindcss",
-			"yamlls",
-			"lua_ls",
-			"clangd",
-			"ruff",
-		},
-	},
+	opts = function()
+		local vue_language_server_path = vim.fn.expand("$MASON/packages")
+			.. "/vue-language-server"
+			.. "/node_modules/@vue/language-server"
+
+		local tsserver_filetypes = {
+			"typescript",
+			"javascript",
+			"javascriptreact",
+			"typescriptreact",
+			"vue",
+		}
+
+		local vue_plugin = {
+			name = "@vue/typescript-plugin",
+			location = vue_language_server_path,
+			languages = { "vue" },
+			configNamespace = "typescript",
+		}
+
+		local ts_ls_config = {
+			init_options = {
+				plugins = {
+					vue_plugin,
+				},
+			},
+			filetypes = tsserver_filetypes,
+		}
+
+		vim.lsp.config("vue_ls", {})
+		vim.lsp.config("ts_ls", ts_ls_config)
+		vim.lsp.enable({ "ts_ls", "vue_ls" })
+
+		return {
+			ensure_installed = {
+				"solargraph",
+				"rubocop",
+				"vue_ls",
+				"ts_ls",
+				"pyright",
+				"zls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"yamlls",
+				"lua_ls",
+				"clangd",
+				"ruff",
+			},
+		}
+	end,
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
