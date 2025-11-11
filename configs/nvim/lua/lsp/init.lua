5141ba56449705d7ea9ff05a2a65b3c4901c3ceb
gugulenok <terzi.kirill.ceiti@gmail.com>
2025-11-11 23:30:12 +0200

    Updated ghostty shader & added Vue LSP

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -12,3 +12,29 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 	end,
 })
+
+-- ==================== [ Vue LSP setup ] ====================
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+	.. "/vue-language-server"
+	.. "/node_modules/@vue/language-server"
+
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_language_server_path,
+	languages = { "vue" },
+	configNamespace = "typescript",
+}
+
+local ts_ls_config = {
+	init_options = {
+		plugins = {
+			vue_plugin,
+		},
+	},
+	filetypes = tsserver_filetypes,
+}
+
+vim.lsp.config("ts_ls", ts_ls_config)
+-- ===========================================================
